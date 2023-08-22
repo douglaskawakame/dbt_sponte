8d74c1109dcd2a358316f8da2eb34acff1d759a6
Douglas Kawakame <douglaskawakame@gmail.com>
2023-08-21 21:10:40 -0300

    commit 5

--- dbt_sponte_project/models/example/teste1.sql
+++ dbt_sponte_project/models/example/teste1.sql
@@ -4,9 +4,9 @@
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
 -- depends_on: {{ source('bi_sponte', '_airbyte_raw_contacts') }}
 select c.label , a.id , b.dealname, b.nome_fantasia,  b.data_ganhou, b.data_sql , b.data_mql, b.codigo_do_cliente 
-From BI_Sponte.deals a
-left join BI_Sponte.deals_properties b on a._airbyte_deals_hashid = b._airbyte_deals_hashid
-left join BI_Sponte.deal_pipelines c on c.pipelineid = b.pipeline 
+From bi_sponte.deals a
+left join bi_sponte.deals_properties b on a._airbyte_deals_hashid = b._airbyte_deals_hashid
+left join bi_sponte.deal_pipelines c on c.pipelineid = b.pipeline 
 where b.data_mql >= '2023-01-01'
-order by b.data_ganhou desc
+
 
